b91ba6b1f49d729a73230642f5cbb8203735df82
Rhugved <kadamrhugved@gmail.com>
2024-03-13 17:37:46 +0530

    firstcommit

---
+++ registrationconnection.php
@@ -0,0 +1,22 @@
+<?php
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+//Database connection
+$conn = new mysqli('localhost', 'root', '', 'project_db');
+if($conn->connect_error){
+    die('Connection Failed :'.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into project_table( firstName, lastName, email, password) values(?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $firstName, $lastName, $email, $password);
+    $stmt->execute();
+    echo "Registration Successfull";
+    $stmt->close();
+    $conn->close();
+
+}
+
+
+
+?>
